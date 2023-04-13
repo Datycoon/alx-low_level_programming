76c12837d5df7d5df200ce6725bca8a4dd4543c6
Datycoon <escowebs@gmail.com>
2023-04-13 14:35:31 -0700

    mul fixed

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -83,44 +83,44 @@ int _checknum(char *argv[], int n)
  */
 int main(int argc, char *argv[])
 {
-	int ln1, ln2, ls, add, addl, i, j, k, ca;
-	char *s;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-	_is_zero(argv), ls = ln1 + ln2, s = malloc(ls + 1);
-	if (s == NULL)
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
 		printf("Error\n"), exit(98);
-	s = _initialize_array(s, ls);
-	k = ls - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
 		if (i < 0)
 		{
 			if (addl > 0)
 			{
-				add = (s[k] - '0') + addl;
+				add = (nout[k] - '0') + addl;
 				if (add > 9)
-					s[k - 1] = (add / 10) + '0';
-				s[k] = (add % 10) + '0';
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
 			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = ls - (1 + ca);
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
 		if (j < 0)
 		{
-			if (s[0] != '0')
+			if (nout[0] != '0')
 				break;
-			ls--;
-			free(s), s = malloc(ls + 1), s = _initialize_array(s, ls);
-			k = ls - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
 		if (j >= 0)
 		{
-			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (s[k] - '0') + addl;
-			addl = add / 10, s[k] = (add % 10) + '0';
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
-	printf("%s\n", s);
+	printf("%s\n", nout);
 	return (0);
 }
