6eb9b32e701ccf7fab9c18fd4925a222a5897c4d
Datycoon <escowebs@gmail.com>
2023-04-13 18:43:17 -0700

    mullum1

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,6 +3,14 @@
 #include <stdio.h>
 #include <ctype.h>
 
+
+
+int find_len(char *str);
+char *create_xarray(int size);
+char *iterate_zeroes(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_nums(char *final_prod, char *next_prod, int next_len);
+
 /**
  * _is_zero - determines if any number is zero
  * @argv: argument vector.
