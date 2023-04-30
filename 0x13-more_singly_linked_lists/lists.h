e9ccdb34af23ed05f91a47aaa36dd31cef77b674
Datycoon <escowebs@gmail.com>
2023-04-30 09:42:59 -0700

    fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,17 +1,9 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * singly linked list node structure
- * 
- */
 
 typedef struct listint_s
 {
