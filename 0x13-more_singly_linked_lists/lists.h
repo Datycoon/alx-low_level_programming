10d2550f883371c4f9036922111825526fe5f8ad
Datycoon <escowebs@gmail.com>
2023-04-30 09:46:37 -0700

    fix error

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,17 +5,11 @@
 #include <stdlib.h>
 
 
-typedef struct listint_s
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
 
 /**
  * struct listp_s - singly linked list
  * @p: pointers of nodes
  * @next: points to the next node
- *
  * Description: singly linked list of pointers
  */
 typedef struct listp_s
