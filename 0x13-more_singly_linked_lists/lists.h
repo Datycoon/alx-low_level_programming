022e8b3b6ee94788d683654f8e8026e62d6b1501
Datycoon <escowebs@gmail.com>
2023-04-30 09:48:33 -0700

    fix er

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,28 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for  project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 /**
  * struct listp_s - singly linked list
  * @p: pointers of nodes
  * @next: points to the next node
+ *
  * Description: singly linked list of pointers
  */
 typedef struct listp_s
