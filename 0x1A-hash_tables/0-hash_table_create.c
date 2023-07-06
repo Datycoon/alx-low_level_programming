1aca55baa35245ae2c6ae5dc2c71402a8e44bbdd
Datycoon <escowebs@gmail.com>
2023-07-06 11:39:17 -0700

    hash fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,25 +1,30 @@
 #include "hash_tables.h"
+
 /**
-* hash_table_create - creates a hash table
-*
-* @size: the size of the table
-* Return: returns the table, or NULL on fail
-*/
+ * hash_table_create - creates a hash table with a given size
+ *
+ * @size: size of the hash table
+ * Return: the created hash table, or NULL if function fails
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = NULL;
+	hash_table_t *table;
+	hash_node_t **array;
+	unsigned long int i;
 
-	if (size == 0)
-		return (NULL);
 	table = malloc(sizeof(hash_table_t));
-	if (!table)
+	if (table == NULL)
 		return (NULL);
-	(*table).size = size;
-	(*table).array = malloc(sizeof(hash_node_t *) * size);
-	if (!(*table).array)
-	{
-		free(table);
+
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
 		return (NULL);
-	}
+
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
+
+	table->array = array;
+	table->size = size;
+
 	return (table);
 }
